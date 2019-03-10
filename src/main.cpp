4e4c9128230080de26671f158235a26cab6b98e0
Rudnitskiy Anton <inapik@Rudnitskiys-Mac.local>
2019-03-10 15:09:54 +0300

    mac os

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,10 @@ using namespace std;
 
 int main()
 {
-  std::thread thr_send = std::thread(ospf_send);
-  std::thread thr_recv_allospf = std::thread(ospf_recv_allospf);
-  std::thread thr_recv_alld = std::thread(ospf_recv_dospf);
-  thr_send.join();
-  thr_recv_allospf.join();
-  thr_recv_alld.join();
+    std::thread thr_send = std::thread(ospf_send);
+    std::thread thr_recv_allospf = std::thread(ospf_recv_allospf);
+    std::thread thr_recv_alld = std::thread(ospf_recv_dospf);
+    thr_send.join();
+    thr_recv_allospf.join();
+    thr_recv_alld.join();
 }
